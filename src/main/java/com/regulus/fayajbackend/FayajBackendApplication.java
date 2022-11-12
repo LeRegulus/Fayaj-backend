f9f5cd223ac0c0082b70e7d5929cf8f3b5b0c1ca
Regulus <alassaneseck94@gmail.com>
2022-11-12 13:42:55 +0000

    First commit

---
+++ src/main/java/com/regulus/fayajbackend/FayajBackendApplication.java
@@ -0,0 +1,13 @@
+package com.regulus.fayajbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FayajBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FayajBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/regulus/fayajbackend/FayajBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regulus.fayajbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FayajBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
